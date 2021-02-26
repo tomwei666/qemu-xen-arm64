3fe3a80860f257969415cc33e089649270932a57
tom-wei <tomwei666@gmail.com>
2021-02-26 10:03:39 +0800

    重新更新2!

--- qemuxen.sh
+++ qemuxen.sh
@@ -39,14 +39,14 @@ unzip_source()
 	# copy
 	cp ${DL}/${BUSYBOX_SOURCE_NAME} ${BUILD}
 	cp ${DL}/${KERNEL_SOURCE_NAME} ${BUILD}
-	cp ${DL}/${XEN_SOURCE_NAME} ${BUILD}
+	#cp ${DL}/${XEN_SOURCE_NAME} ${BUILD}
 	cp ${DL}/${UBOOT_SOURCE_NAME} ${BUILD}
 
 	cd ${BUILD}
 
 	tar -jxv -f ${BUSYBOX_SOURCE_NAME}
 	tar -Jxv -f ${KERNEL_SOURCE_NAME}
-	tar -zxv -f ${XEN_SOURCE_NAME}
+	#tar -zxv -f ${XEN_SOURCE_NAME}
 	tar -jxv -f ${UBOOT_SOURCE_NAME}
 
 }
